9b4a33ef87b83356fa129142bf4a2d0b9c26de7f
Diana_Bobrova <dziana_babrova@epam.com>
2022-08-26 22:29:25 +0300

    feat: add shuffling

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const ancientsHeader = document.querySelector(".ancients-header");
 const cardCounts = document.querySelectorAll(".stage-item");
 const schemeElement = document.querySelector(".scheme");
 const complexity = document.querySelector(".complexity");
+const levels = document.querySelectorAll(".level");
 
 let enemy;
 let scheme = [
@@ -17,7 +18,14 @@ let scheme = [
   [0, 0, 0]
 ];
 let flattedScheme;
-let chosenComplexity = "Very easy";
+let chosenComplexity;
+let greenCardsForGame;
+let brownCardsForGame;
+let blueCardsForGame;
+let stage1Cards = [];
+let stage2Cards = [];
+let stage3Cards = [];
+let cardStack = [];
 
 ancient.forEach((el, index) => {
   el.addEventListener("click", function zoomAncient(e) {
@@ -34,11 +42,10 @@ ancient.forEach((el, index) => {
       schemeElement.classList.add("scheme-visible");
       complexity.classList.remove("complexity-hidden");
       
-        enemy = ancientsData[index];
-        applyAncientsHeader();
+      enemy = ancientsData[index];
+      applyAncientsHeader();
       createGameSchemeArray();
       createGameScheme();
-      pickCards();
     }
   });
 });
@@ -49,12 +56,14 @@ closeButtons.forEach((el, index) => {
     ancient.forEach((el) => el.classList.remove("inactive"));
     closeButtons[index].classList.add("close-hidden");
     schemeElement.classList.remove("scheme-visible");
-      complexity.classList.add("complexity-hidden");
-      enemy = ancientsData[-1];
-      applyAncientsHeader();
+    complexity.classList.add("complexity-hidden");
+    enemy = ancientsData[-1];
+    chosenComplexity = -1;
+    pickCards();
+    applyAncientsHeader();
     createGameSchemeArray();
     createGameScheme();
-pickCards();
+    levels.forEach((el) => el.classList.remove("level-chosen"));
   });
 });
 
@@ -110,45 +119,122 @@ function pickCards() {
   let normalGreen = green.filter((el) => el.difficulty === "normal");
   let hardGreen = green.filter((el) => el.difficulty === "hard");
 
-  if (chosenComplexity === "Very easy") {
-    let easyGreenNumber = scheme[0][0] + scheme[1][0] + scheme[2][0];
-    let newEasyGreen = easyGreen;
-    let newNormalGreen = normalGreen;
-    while (easyGreenNumber > newEasyGreen.length) {
-      let randomNumber = Math.floor(Math.random() * (newNormalGreen.length - 1));
-      let newCard = newNormalGreen[randomNumber];
-      newEasyGreen.push(newCard);
-      newNormalGreen.splice(randomNumber, 1);
-    }
-    let easyBrownNumber = scheme[0][1] + scheme[1][1] + scheme[2][1];
-    let newEasyBrown = easyBrown;
-    let newNormalBrown = normalBrown;
-    while (easyBrownNumber > newEasyBrown.length) {
-      let randomNumber = Math.floor(Math.random() * (newNormalBrown.length - 1));
-      let newCard = newNormalBrown[randomNumber];
-      newEasyBrown.push(newCard);
-      newNormalBrown.splice(randomNumber, 1);
-    }
-        let easyBlueNumber = scheme[0][2] + scheme[1][2] + scheme[2][2];
-        let newEasyBlue = easyBlue;
-        let newNormalBlue = normalBlue;
-        while (easyBlueNumber > newEasyBlue.length) {
-          let randomNumber = Math.floor(Math.random() * (newNormalBlue.length - 1));
-          let newCard = newNormalBlue[randomNumber];
-          newEasyBlue.push(newCard);
-          newNormalBlue.splice(randomNumber, 1);
-    }
-  } else if (chosenComplexity === "Easy") {
+  let GreenNumber = scheme[0][0] + scheme[1][0] + scheme[2][0];
+  let BrownNumber = scheme[0][1] + scheme[1][1] + scheme[2][1];
+  let BlueNumber = scheme[0][2] + scheme[1][2] + scheme[2][2];
 
-  } else if (chosenComplexity === "Normal") {
+  if (chosenComplexity === 0) {
+    greenCardsForGame = Array.from(easyGreen);
+    let newNormalGreenPack = Array.from(normalGreen);
 
-  } else if (chosenComplexity === "Hard") {
+    addNewCard(GreenNumber, greenCardsForGame, newNormalGreenPack);
 
-  } else if (chosenComplexity === "Very hard") {
+    brownCardsForGame = Array.from(easyBrown);
+    let newNormalBrownPack = Array.from(normalBrown);
+
+    addNewCard(BrownNumber, brownCardsForGame, newNormalBrownPack);
+
+    blueCardsForGame = Array.from(easyBlue);
+    let newNormalBluePack = Array.from(normalBlue);
+
+    addNewCard(BlueNumber, blueCardsForGame, newNormalBluePack);
+    
+  } else if (chosenComplexity === 1) {
+    greenCardsForGame = easyGreen.concat(normalGreen);
+    brownCardsForGame = easyBrown.concat(normalBrown);
+    blueCardsForGame = easyBlue.concat(normalBlue);
+  } else if (chosenComplexity === 2) {
+    greenCardsForGame = Array.from(green);
+    brownCardsForGame = Array.from(brown);
+    blueCardsForGame = Array.from(blue);
+  } else if (chosenComplexity === 3) {
+    greenCardsForGame = normalGreen.concat(hardGreen);
+    brownCardsForGame = normalBrown.concat(hardBrown);
+    blueCardsForGame = normalBlue.concat(hardBlue);
+  } else if (chosenComplexity === 4) {
+    greenCardsForGame = Array.from(hardGreen);
+    let newNormalGreenPack = Array.from(normalGreen);
+
+    addNewCard(GreenNumber, greenCardsForGame, newNormalGreenPack);
+    
+    brownCardsForGame = Array.from(hardBrown);
+    let newNormalBrownPack = Array.from(normalBrown);
+
+    addNewCard(BrownNumber, brownCardsForGame, newNormalBrownPack);
+
+    blueCardsForGame = Array.from(hardBlue);
+    let newNormalBluePack = Array.from(normalBlue);
+
+    addNewCard(BlueNumber, blueCardsForGame, newNormalBluePack);
 
   } else {
-
+    blueCardsForGame = [];
+    greenCardsForGame = [];
+    brownCardsForGame = [];
   }
 }
 
-pickCards();
+function addNewCard(number, cardsForGame, newPack) {
+      while (number > cardsForGame.length) {
+        let randomNumber = Math.floor(Math.random() * (newPack.length - 1));
+        let newCard = newPack[randomNumber];
+        cardsForGame.push(newCard);
+        newPack.splice(randomNumber, 1);
+      }
+}
+
+levels.forEach((el, index) => {
+  el.addEventListener("click", function () {
+    chosenComplexity = index;
+    levels.forEach(el => el.classList.remove("level-chosen"));
+    levels[index].classList.add("level-chosen");
+    stage1Cards = [];
+    stage2Cards = [];
+    stage3Cards = [];
+    pickCards();
+
+    let shuffledGreenCards = shuffle(greenCardsForGame);
+    let shuffledBrownCards = shuffle(brownCardsForGame);
+    let shuffledBlueCards = shuffle(blueCardsForGame);
+
+    pickCardForStage(scheme[0][0], shuffledGreenCards, stage1Cards);
+    pickCardForStage(scheme[0][1], shuffledBrownCards, stage1Cards);
+    pickCardForStage(scheme[0][2], shuffledBlueCards, stage1Cards);
+    pickCardForStage(scheme[1][0], shuffledGreenCards, stage2Cards);
+    pickCardForStage(scheme[1][1], shuffledBrownCards, stage2Cards);
+    pickCardForStage(scheme[1][2], shuffledBlueCards, stage2Cards);
+    pickCardForStage(scheme[2][0], shuffledGreenCards, stage3Cards);
+    pickCardForStage(scheme[2][1], shuffledBrownCards, stage3Cards);
+    pickCardForStage(scheme[2][2], shuffledBlueCards, stage3Cards);
+
+    stage1Cards = shuffle(stage1Cards);
+    stage2Cards = shuffle(stage2Cards);
+    stage3Cards = shuffle(stage3Cards);
+    
+    console.log(stage1Cards);
+    console.log(stage2Cards);
+    console.log(stage3Cards);
+
+    cardStack = stage3Cards.concat(stage2Cards, stage1Cards);
+    console.log(cardStack);
+  })
+})
+
+function shuffle(array) {
+  let newDeck = Array.from(array);
+for (let i = newDeck.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  const temp = newDeck[i];
+  newDeck[i] = newDeck[j];
+  newDeck[j] = temp;
+  }  
+  return newDeck;
+}
+
+function pickCardForStage(number, source, target) {
+  for (let i = 1; i <= number; i++) {
+    let randomNumber = Math.floor(Math.random() * (source.length - 1));
+    target.push(source[randomNumber]);
+    source.splice(randomNumber, 1);
+  }
+}
